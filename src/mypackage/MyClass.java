f68e0ace0c7949f4b899e6fbb8bcb3948dcff0dd
Halil Ibrahim <hisetkal@gmail.com>
2020-06-20 09:38:19 -0400

    saome updates

--- src/mypackage/MyClass.java
+++ src/mypackage/MyClass.java
@@ -2,6 +2,8 @@ package mypackage;
 
 public class MyClass {
 public static void main(String[] args) {
+	System.out.println("Veli");
+	System.out.println("Hasan");
 	System.out.println("Mahmut");
 	System.out.println("Osman");
 	
